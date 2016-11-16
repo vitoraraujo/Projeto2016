091d7a69f26f51cbfc3973fc4ccf7b7d4ede7d20
vitoraraujo <vitoraraujow@gmail.com>
2016-11-16 13:02:27 -0200

    will_paginate e linker no quadrado

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'tinymce-rails'
+gem 'will_paginate', '~> 3.1'
+gem 'will_paginate-bootstrap', '~> 1.0', '>= 1.0.1'
 
 group :development, :test do
   gem 'sqlite3'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :checkUser, only: [:edit, :update, :destroy]
   def index
-    @posts = Post.all
+    @posts = Post.all.paginate(page: params[:page], per_page: 3)
   end
 
   def search
